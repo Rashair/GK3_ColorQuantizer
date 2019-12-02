9a13c04b160bdc46147d35974555371ddc0ad22f
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-12-02 19:36:03 +0100

    Refactoring of popularity algorithm

--- GK3_ColorQuantizer/Algorithms/PopularityAlgorithm.cs
+++ GK3_ColorQuantizer/Algorithms/PopularityAlgorithm.cs
@@ -18,6 +18,8 @@ namespace GK3_ColorQuantizer.Algorithms
             indexes = Enumerable.Range(0, 256 * 256 * 256).ToArray();
         }
 
+        private int[] currentPalette;
+
         public PopularityAlgorithm(WriteableBitmap bitmap)
         {
             this.Bitmap = bitmap;
@@ -28,12 +30,13 @@ namespace GK3_ColorQuantizer.Algorithms
                 colorCounts[col] += 1;
             }
 
-            Array.Sort(colorCounts, indexes);
+            Array.Sort(colorCounts, indexes, Comparer<int>.Create((a, b) => b.CompareTo(a)));
         }
 
         public override void Apply(int Kr, int Kg, int Kb)
         {
             int K = Kr;
+            currentPalette = indexes.Take(K).ToArray();
 
             Bitmap.Lock();
             unsafe
@@ -45,7 +48,7 @@ namespace GK3_ColorQuantizer.Algorithms
                     for (int j = 0; j < width; ++j)
                     {
                         int color = (originalCopy[copyIt] << 16) | (originalCopy[copyIt + 1] << 8) | (originalCopy[copyIt + 2]);
-                        var (B, G, R) = GetRGB(GetNearestColor(color, K));
+                        var (B, G, R) = GetNearestColor(color);
                         currPos[0] = B.ToByte();
                         currPos[1] = G.ToByte();
                         currPos[2] = R.ToByte();
@@ -59,54 +62,47 @@ namespace GK3_ColorQuantizer.Algorithms
             Bitmap.Unlock();
         }
 
-        private int GetNearestColor(int color, int K)
+        private (int B, int G, int R) GetNearestColor(int color)
         {
-            // initializes the best difference, set it for worst possible, it can only get better
             int bestColor = indexes[0];
-            int leastDifference = int.MaxValue;
+            int minDifference = int.MaxValue;
 
-            var (colorB, colorG, colorR) = GetRGB(color);
-            for (int i = 0; i < K; ++i)
+            var (Bcolor, Gcolor, Rcolor) = GetRGB(color);
+            for (int i = 0; i < currentPalette.Length; ++i)
             {
-                int targetColor = indexes[i];
+                int targetColor = currentPalette[i];
                 var (B, G, R) = GetRGB(targetColor);
 
-                // calculates a difference for all the color components
-                int deltaR = R - colorR;
-                int deltaG = G - colorG;
-                int deltaB = B - colorB;
-
-                // calculates a power of two
-                int factorR = deltaR * deltaR;
-                int factorG = deltaG * deltaG;
-                int factorB = deltaB * deltaB;
-
-                // calculates the Euclidean distance, a square-root is not need 
-                // as we're only comparing distance, not measuring it
-                int difference = factorR + factorG + factorB;
-
+                int dist = GetDistanceBetweenColors(R, Rcolor, G, Gcolor, B, Bcolor);
                 // if a difference is zero, we're good because it won't get better
-                if (difference == 0)
+                if (dist == 0)
                 {
-                    bestColor = indexes[i];
+                    bestColor = currentPalette[i];
                     break;
                 }
 
-                // if a difference is the best so far, stores it as our best candidate
-                if (difference < leastDifference)
+                if (dist < minDifference)
                 {
-                    leastDifference = difference;
-                    bestColor = indexes[i];
+                    minDifference = dist;
+                    bestColor = currentPalette[i];
                 }
             }
 
-            // returns the palette index of the most similar color
-            return bestColor;
+            return GetRGB(bestColor);
         }
 
         private (int B, int G, int R) GetRGB(int col)
         {
             return ((col >> 16) & 0xFF, (col >> 8) & 0xFF, col & 0xFF);
+        }
+
+        private int GetDistanceBetweenColors(int R1, int R2, int G1, int G2, int B1, int B2)
+        {
+            int deltaR = R1 - R2;
+            int deltaG = G1 - G2;
+            int deltaB = B1 - B2;
+
+            return deltaR * deltaR + deltaG * deltaG + deltaB * deltaB;
         }
     }
 }
